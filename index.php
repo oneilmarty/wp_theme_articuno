6b512c6ad243b2f86cb0c7e9a96e22643870c5f8
Marty O'Neil <oneilmarty@gmail.com>
2017-09-14 13:58:03 -0500

    WP Communication
    Added dynamic functions for titles, links, and content.

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>Hello World</h1>
+<?php
+
+	if (have_posts()) :
+		while (have_posts()) : the_post(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_title(); ?>
+		<?php endwhile;
+	else :
+		echo "<p>no content found</p>";
+	endif;
+?>
